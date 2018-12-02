d30ae17c809fa0f9e5c8d0f65225eec3f4ccaa87
Yunyao Yao <yao.yao13@det.nsw.edu.au>
2018-12-03 10:49:28 +1100

    added preferenceService

--- alan.service.js
+++ alan.service.js
@@ -1,8 +1,8 @@
-module.exports = function(app){
+module.exports = function(app) {
 	app.factory('ALANServices', ['$q', '$http', function($q, $http) {
 		return {
 			sendReq: function(method, url, data, options) {
-	
+
 				return $q(function(resolve, reject) {
 					$http(Object.assign({
 						method: method,
@@ -19,15 +19,20 @@ module.exports = function(app){
 			contextService: function() {
 				return this.sendReq('GET', '/security/context');
 			},
-	
+
 			authCheck: function() {
 				return this.sendReq('GET', '/security/auth');
 			},
-	
+
 			logOut: function() {
 				return this.sendReq('POST', '/security/logout');
 			},
-	
+			getPreference: function() {
+				return this.sendReq('GET', '/user/preferences/', null, {});
+			},
+			setPreference: function(data) {
+				return this.sendReq('POST', '/user/preferences/', data, {});
+			},
 			checkSession: function() {
 				return $q(function(resolve, reject) {
 					var msg = 'You have been logged out due to inactivity.\nPlease refresh your session to log in again.';
